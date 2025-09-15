9a5bc2045cae5dfd324beff5420c2a3a88889411
TobiEGA <tobias.v@egafutura.com>
2025-09-15 16:18:04 -0300

    TEST CORRECION

--- force-app/main/default/lwc/languageSelector/languageSelector.js
+++ force-app/main/default/lwc/languageSelector/languageSelector.js
@@ -4,29 +4,29 @@ import UKFlag from '@salesforce/resourceUrl/UKFlag';
 import BrazilFlag from '@salesforce/resourceUrl/BrazilFlag';
 
 export default class LanguageSelector extends LightningElement {
-    @api Seleccionar_tu_idioma_new; 
+    @api Seleccionar_tu_idioma; 
 
     argentinaSrc = ArgentinaFlag;
     ukSrc = UKFlag;
     brazilSrc = BrazilFlag;
 
     get isSpanishSelected() {
-        return this.Seleccionar_tu_idioma_new === 'Spanish';
+        return this.Seleccionar_tu_idioma === 'Spanish';
     }
 
     get isEnglishSelected() {
-        return this.Seleccionar_tu_idioma_new === 'English';
+        return this.Seleccionar_tu_idioma === 'English';
     }
 
     get isPortugueseSelected() {
-        return this.Seleccionar_tu_idioma_new === 'Portuguese';
+        return this.Seleccionar_tu_idioma === 'Portuguese';
     }
 
     handleLanguageChange(event) {
-        this.Seleccionar_tu_idioma_new = event.target.value;
+        this.Seleccionar_tu_idioma = event.target.value;
 
         try {
-            sessionStorage.setItem('idiomaSeleccionado', this.Seleccionar_tu_idioma_new);
+            sessionStorage.setItem('idiomaSeleccionado', this.Seleccionar_tu_idioma);
         } catch (error) {
             console.warn('SessionStorage no disponible:', error);
         }
